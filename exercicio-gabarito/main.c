ff38e9b3905af46558b71ab7b2c738855f38c32a
Drsdavi62 <davi.ribeirosantos11@gmail.com>
2020-06-28 11:52:26 -0300

    Projeto gabarito

--- exercicio-gabarito/main.c
+++ exercicio-gabarito/main.c
@@ -2,26 +2,54 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main() {
-    FILE *fResp, *fGab, *fResult;
-    char ra;
-    char resp[101];
+int main()
+{
+    FILE *fResp, *fGab, *fResult, *fResultFormat;
+    int ra;
+    int comp = 0;
+    char resp[101] = "";
     char gab[101];
     char * respostas = "./resources/respostas.txt";
     char * gabarito = "./resources/gabarito.txt";
     char * resultado = "./resources/resultado.txt";
+    char * resultadoFormatado = "./resources/resultadoFormatado.txt";
 
     fResp = fopen(respostas, "r");
     fGab = fopen(gabarito, "r");
     fResult = fopen(resultado, "w");
+    fResultFormat = fopen(resultadoFormatado, "w");
+
+    fgets(gab, 100, fGab);
 
     while (fgets(resp, 1000, fResp) != NULL){
-        char aux[256];
-        printf("%s \n", strncpy(aux, resp, 100));
-//        while (c < length) {
-//            sub[c] = string[position+c-1];
-//            c++;
-//        }
+        char * token = strtok(resp, " ");
+       int index = 0;
+        while( token != NULL ) {
+            if(index == 0){
+                fprintf(fResultFormat, "Ra: %s \n", token );
+                fputs(token, fResult);
+            }else {
+                fprintf(fResultFormat, "Resposta: %s \n", token );
+                for(int i = 0; i < strlen(token); i++){
+                    if(gab[i] == token[i]){
+                        comp++;
+                    }
+                }
+                char res[50];
+                snprintf(res, 50, "  %d", comp);
+                fprintf(fResultFormat, "Resultado: %d \n\n", comp);
+                fputs(res, fResult);
+                comp = 0;
+                fputs("\n", fResult);
+            }
+            token = strtok(NULL, "\n");
+            index++;
+        }
     }
+
+    fclose(fResp);
+    fclose(fGab);
+    fclose(fResult);
+
     return 0;
 }
